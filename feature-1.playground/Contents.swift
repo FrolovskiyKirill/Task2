a6e2b3d175900b97c2db32fece844f89fc71c9e2
FrolovskiyKirill <97293194+FrolovskiyKirill@users.noreply.github.com>
2022-12-26 23:03:55 +0300

    feat: add third task

--- feature-1.playground/Contents.swift
+++ feature-1.playground/Contents.swift
@@ -34,3 +34,22 @@ func fibonacciSeriese(num: Int) {
 }
 
 fibonacciSeriese(num: 15)
+
+//Bubble Sort
+func bubbleSort(_ array: [Int]) -> [Int] {
+
+    var sortedArray = array
+    
+    for i in 0..<sortedArray.count {
+        for j in 0..<sortedArray.count - i - 1 {
+            if sortedArray[j] > sortedArray[j + 1] {
+                sortedArray.swapAt(j + 1, j)
+            }
+        }
+    }
+    return sortedArray
+}
+
+let someNumbers = [6, 3, 5, -2, 19, 7, -8]
+
+bubbleSort(someNumbers)
